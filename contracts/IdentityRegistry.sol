d83d196dd2002771dfd025a7024fec80e0a4809c
v3nom-95 <user@example.com>
2026-04-14 14:02:16 +0530

    Add/Update contracts/IdentityRegistry.sol

--- contracts/IdentityRegistry.sol
+++ contracts/IdentityRegistry.sol
@@ -2,10 +2,27 @@
 pragma solidity ^0.8.20;
 
 contract IdentityRegistry {
+    address public owner;
     mapping(address => bytes32) private identityHashes;
     event IdentityRegistered(address indexed user, bytes32 didHash);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    function registerIdentity(address user, bytes32 didHash) external {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Invalid owner");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function registerIdentity(address user, bytes32 didHash) external onlyOwner {
         require(user != address(0), "Invalid user");
         require(didHash != bytes32(0), "Invalid DID hash");
         identityHashes[user] = didHash;
