1ed69d2bec3fd8a3aaa1e3d8a8aeeb39e65dc09a
v3nom-95 <user@example.com>
2026-04-14 14:02:16 +0530

    Add/Update contracts/RiskRegistry.sol

--- contracts/RiskRegistry.sol
+++ contracts/RiskRegistry.sol
@@ -2,10 +2,27 @@
 pragma solidity ^0.8.20;
 
 contract RiskRegistry {
+    address public owner;
     mapping(address => uint256) private riskScores;
     event RiskScoreStored(address indexed user, uint256 score);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    function storeRiskScore(address user, uint256 score) external {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Invalid owner");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function storeRiskScore(address user, uint256 score) external onlyOwner {
         require(user != address(0), "Invalid user");
         require(score <= 100, "Score out of range");
         riskScores[user] = score;
