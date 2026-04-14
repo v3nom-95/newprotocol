fea027e5e2584fc5cc0387cdb898af7297ca7290
v3nom-95 <user@example.com>
2026-04-14 14:03:57 +0530

    Add/Update sdk/index.js

--- sdk/index.js
+++ sdk/index.js
@@ -1,3 +1,4 @@
 export { createWallet } from "./wallet.js";
 export { sendTransaction, getTransactions } from "./transaction.js";
 export { getRiskScore } from "./risk.js";
+export { QAIError } from "./errors.js";
