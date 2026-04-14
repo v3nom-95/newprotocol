6647895544b78fd11268b50623812db9fca9afa2
v3nom-95 <user@example.com>
2026-04-14 14:02:16 +0530

    Add/Update contracts/TransactionRegistry.sol

--- contracts/TransactionRegistry.sol
+++ contracts/TransactionRegistry.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 contract TransactionRegistry {
+    address public owner;
     struct TransactionMeta {
         bytes pqSignature;
         bytes32 metadataHash;
@@ -10,8 +11,24 @@ contract TransactionRegistry {
 
     mapping(bytes32 => TransactionMeta) private txStore;
     event TransactionStored(bytes32 indexed txHash, bytes pqSignature, bytes32 metadataHash);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    function storeTransaction(bytes32 txHash, bytes calldata pqSignature, bytes32 metadataHash) external {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Invalid owner");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function storeTransaction(bytes32 txHash, bytes calldata pqSignature, bytes32 metadataHash) external onlyOwner {
         require(txHash != bytes32(0), "Invalid tx hash");
         require(metadataHash != bytes32(0), "Invalid metadata hash");
         txStore[txHash] = TransactionMeta({
